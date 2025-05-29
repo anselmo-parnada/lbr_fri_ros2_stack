98943215c4cef0ed92333951d191496b0074ec62
anselmo-parnada <anselmo.parnada@outlook.com>
2025-05-29 15:30:43 +0100

    disabled f/t broadcaster in the hardware bringup as it is not needed

--- lbr_bringup/launch/hardware.launch.py
+++ lbr_bringup/launch/hardware.launch.py
@@ -48,9 +48,9 @@ def generate_launch_description() -> LaunchDescription:
     joint_state_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
         controller="joint_state_broadcaster"
     )
-    force_torque_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
-        controller="force_torque_broadcaster"
-    )
+    # force_torque_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
+    #     controller="force_torque_broadcaster"
+    # )
     lbr_state_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
         controller="lbr_state_broadcaster"
     )
@@ -63,7 +63,7 @@ def generate_launch_description() -> LaunchDescription:
             target_action=ros2_control_node,
             on_start=[
                 joint_state_broadcaster,
-                force_torque_broadcaster,
+                # force_torque_broadcaster,
                 lbr_state_broadcaster,
                 controller,
             ],
